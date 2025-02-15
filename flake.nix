a2c0802d008c228e6c6dfffa3544ba3a261c893a
greysilly7 <greysilly7@gmail.com>
2025-02-14 23:43:16 -0500

    Update Rust edition to 2021 and simplify flake.nix configuration
    Signed-off-by: greysilly7 <greysilly7@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,80 +1,29 @@
 {
-  description = "Rust-Nix";
-
   inputs = {
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    crate2nix.url = "github:nix-community/crate2nix";
-
-    # Development
-
-    devshell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
   };
 
-  nixConfig = {
-    extra-trusted-public-keys = "eigenvalue.cachix.org-1:ykerQDDa55PGxU25CETy9wF6uVDpadGGXYrFNJA3TUs=";
-    extra-substituters = "https://eigenvalue.cachix.org";
-    allow-import-from-derivation = true;
-  };
-
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    flake-parts,
-    rust-overlay,
-    crate2nix,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
-      imports = [
-        ./nix/rust-overlay/flake-module.nix
-        ./nix/devshell/flake-module.nix
-      ];
-
-      perSystem = {
-        system,
-        pkgs,
-        lib,
-        inputs',
-        ...
-      }: let
-        # If you dislike IFD, you can also generate it with `crate2nix generate`
-        # on each dependency change and import it here with `import ./Cargo.nix`.
-        cargoNix = inputs.crate2nix.tools.${system}.appliedCargoNix {
-          name = "rustnix";
-          src = ./.;
-        };
-      in rec {
-        checks = {
-          rustnix = cargoNix.rootCrate.build.override {
-            runTests = true;
+  outputs = inputs:
+    with inputs;
+      flake-utils.lib.eachDefaultSystem (
+        system: let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [cargo2nix.overlays.default];
           };
-        };
 
-        packages = {
-          rustnix = cargoNix.rootCrate.build;
-          default = packages.rustnix;
-
-          inherit (pkgs) rust-toolchain;
-
-          rust-toolchain-versions = pkgs.writeScriptBin "rust-toolchain-versions" ''
-            ${pkgs.rust-toolchain}/bin/cargo --version
-            ${pkgs.rust-toolchain}/bin/rustc --version
-          '';
-        };
-      };
-    };
+          rustPkgs = pkgs.rustBuilder.makePackageSet {
+            rustVersion = "1.75.0";
+            packageFun = import ./Cargo.nix;
+          };
+        in rec {
+          packages = {
+            # replace hello-world with your package name
+            mc_tools = rustPkgs.workspace.mc_tools {};
+            default = packages.hello-world;
+          };
+        }
+      );
 }
